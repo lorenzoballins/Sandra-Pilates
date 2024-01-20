171a64a3517cc06794d6cf992d1df30ad8044498
Lorenzo Stoco <154258437+lorenzoballins@users.noreply.github.com>
2024-01-20 14:14:05 -0300

    adicionar pasta Project-Sandra

--- contato.js
+++ contato.js
@@ -1,63 +1,59 @@
 document.addEventListener('DOMContentLoaded', function () {
     const formulario = document.getElementById('formulario');
+    let envioEmProgresso = false;
 
     formulario.addEventListener('submit', async function (event) {
         event.preventDefault();
+
         document.getElementById('botao-enviar').disabled = true;
 
-        await enviarFormulario();
+        try {
+            await enviarFormulario();
+
+            alert('Formulário enviado com sucesso! Entraremos em contato em breve.');
+            document.getElementById('formulario').reset();
+        } catch (error) {
+            console.error('Erro ao enviar o formulário:', error.message);
+            alert('Ocorreu um erro ao enviar o formulário. Por favor, tente novamente mais tarde.');
+        } finally {
+            document.getElementById('botao-enviar').disabled = false;
+        }
     });
 });
 
-let envioEmProgresso = false;
-
 async function enviarFormulario() {
-    try {
-        if (envioEmProgresso) {
-            return;
-        }
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const telefone = document.getElementById('telefone').value;
+    const mensagem = document.getElementById('mensagem').value;
 
-        envioEmProgresso = true;
-
-        const nome = document.getElementById('nome').value;
-        const email = document.getElementById('email').value;
-        const telefone = document.getElementById('telefone').value;
-        const mensagem = document.getElementById('mensagem').value;
-
-        if (!nome || !email || !telefone) {
-            throw new Error('Por favor, preencha todos os campos obrigatórios.');
-        }
-
-        const dados = {
-            nome: nome,
-            email: email,
-            telefone: telefone,
-            mensagem: mensagem
-        };
-
-        // Use a variável de ambiente SERVER_URL
-        const serverUrl = process.env.SERVER_URL || 'https://localhost:3000';
-        const response = await fetch(`${serverUrl}/salvar-dados`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(dados)
-        });
-
-        const data = await response.json();
-
-        if (data.success) {
-            alert('Formulário enviado com sucesso! Entraremos em contato em breve.');
-            document.getElementById('formulario').reset();
-        } else {
-            throw new Error('Erro ao enviar o formulário.');
-        }
-    } catch (error) {
-        console.error('Erro ao enviar o formulário:', error.message);
-        alert('Ocorreu um erro ao enviar o formulário. Por favor, tente novamente mais tarde.');
-    } finally {
-        document.getElementById('botao-enviar').disabled = false;
-        envioEmProgresso = false;
+    if (!nome || !email || !telefone) {
+        throw new Error('Por favor, preencha todos os campos obrigatórios.');
     }
+
+    const dados = {
+        nome: nome,
+        email: email,
+        telefone: telefone,
+        mensagem: mensagem
+    };
+
+    const serverUrl = detectarAmbiente() === 'production' ? 'https://192.168.15.17:3000' : 'http://localhost:3000';
+    const response = await fetch(`${serverUrl}/salvar-dados`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dados)
+    });
+
+    const data = await response.json();
+
+    if (!data.success) {
+        throw new Error('Erro ao enviar o formulário.');
+    }
+}
+
+function detectarAmbiente() {
+    return typeof window !== 'undefined' && window.location.href.startsWith('https://') ? 'production' : 'development';
 }
