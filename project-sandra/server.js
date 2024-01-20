e1eb2f85ebe928f8d930db2eb265afae42d69c3d
Lorenzo Stoco <154258437+lorenzoballins@users.noreply.github.com>
2024-01-19 23:15:51 -0300

    Adicionando novos arquivos

---
+++ project-sandra/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/salvar-dados', (req, res) => {
+    const dados = req.body;
+
+    // Salve os dados localmente
+    salvarDadosLocalmente(dados);
+
+    console.log('Dados recebidos:', dados);
+    res.json({ success: true });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
+function salvarDadosLocalmente(dados) {
+    const dataString = `${new Date().toLocaleString()}\nNome: ${dados.nome}\nEmail: ${dados.email}\nTelefone: ${dados.telefone}\nMensagem: ${dados.mensagem}\n\n`;
+
+    const filePath = path.join(__dirname, 'respostas.txt');
+    
+    fs.writeFile(filePath, dataString, { flag: 'a' }, (err) => {
+        if (err) {
+            console.error('Erro ao salvar os dados localmente:', err);
+        } else {
+            console.log('Dados salvos localmente!');
+        }
+    });
+}
+
