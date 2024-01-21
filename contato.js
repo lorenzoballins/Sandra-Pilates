cbb4812c48e340bd3ef738814b4dbd6e4c9e5dce
Lorenzo Stoco <154258437+lorenzoballins@users.noreply.github.com>
2024-01-21 16:52:39 -0300

    adicionar pasta Project-Sandra

--- contato.js
+++ contato.js
@@ -12,8 +12,13 @@ document.addEventListener('DOMContentLoaded', function () {
             alert('Formulário enviado com sucesso! Entraremos em contato em breve.');
             document.getElementById('formulario').reset();
         } catch (error) {
-            console.error('Erro ao enviar o formulário:', error.message);
-            alert('Ocorreu um erro ao enviar o formulário. Por favor, tente novamente mais tarde.');
+            console.error('Erro ao enviar o formulário:', error);
+
+            if (error.message) {
+                alert(`Ocorreu um erro ao enviar o formulário: ${error.message}`);
+            } else {
+                alert('Ocorreu um erro ao enviar o formulário. Por favor, tente novamente mais tarde.');
+            }
         } finally {
             document.getElementById('botao-enviar').disabled = false;
         }
@@ -37,13 +42,17 @@ async function enviarFormulario() {
         mensagem: mensagem
     };
 
-    const response = await fetch('/salvar-dados', {
+    const response = await fetch('https://localhost:3001/salvar-dados', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(dados)
     });
+
+    if (!response.ok) {
+        throw new Error(`Erro ao enviar o formulário. Status: ${response.status} ${response.statusText}`);
+    }
 
     const data = await response.json();
 
